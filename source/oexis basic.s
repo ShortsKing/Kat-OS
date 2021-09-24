a417257414becf6268c7ef441158e27a65d8f3de
ShortsKing <82279627+ShortsKing@users.noreply.github.com>
2021-09-24 14:19:20 -0500

    Delete oexis basic.s

--- source/oexis basic.s
+++
@@ -1,8 +0,0 @@
-.globl print /* makes function accessable to all scripts */
-.globl setting
-
-print:
-/* put code for function print in here */
-
-setting:
-/* switches the setting from desktop/image to terminal, or vice versa */
