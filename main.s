09d6e258b39d1c64c52a10f1298bc008dec91286
3dcoolcat <82654465+3dcoolcat@users.noreply.github.com>
2021-11-09 15:20:17 -0600

    Update main.s

--- main.s
+++ main.s
@@ -98,3 +98,72 @@ b loop$
 .align 2
 pattern:
 .int 0b11111111101010100010001000101010
+.globl GetMailboxBase
+GetMailboxBase:
+ldr r0,=0x2000B880
+mov pc,lr
+
+.globl MailboxWrite
+MailboxWrite:
+tst r0,#0b1111
+movne pc,lr
+cmp r1,#15
+
+channel .req r1
+value .req r2
+mov value,r0
+push {lr}
+bl GetMailboxBase
+mailbox .req r0
+movhi pc,lr
+
+wait1$:
+status .req r3
+ldr status,[mailbox,#0x18]
+
+tst status,#0x80000000
+.unreq status
+bne wait1$
+
+add value,channel
+.unreq channel
+
+str value,[mailbox,#0x20]
+.unreq value
+.unreq mailbox
+pop {pc}
+
+.globl MailboxRead
+MailboxRead:
+cmp r0,#15
+movhi pc,lr
+
+channel .req r1
+mov channel,r0
+push {lr}
+bl GetMailboxBase
+
+rightmail$:
+wait2$:
+status .req r2
+ldr status,[mailbox,#0x18]
+
+tst status,#0x40000000
+.unreq status
+bne wait2$
+
+mail .req r2
+ldr mail,[mailbox,#0]
+
+inchan .req r3
+and inchan,mail,#0b1111
+teq inchan,channel
+.unreq inchan
+bne rightmail$
+.unreq mailbox
+.unreq channel
+
+and r0,mail,#0xfffffff0
+.unreq mail
+pop {pc}
+mailbox .req r0
