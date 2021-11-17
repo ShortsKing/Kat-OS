a4a3700eeb7dfd388856a5de65b42e572db8c6ae
3dcoolcat <82654465+3dcoolcat@users.noreply.github.com>
2021-11-16 19:06:10 -0600

    Update framebuffer.s

--- framebuffer.s
+++ framebuffer.s
@@ -12,3 +12,45 @@ FrameBufferInfo:
 .int 0 /* #28 Y */
 .int 0 /* #32 GPU - Pointer */
 .int 0 /* #36 GPU - Size */
+
+.section .text
+.globl InitialiseFrameBuffer
+InitialiseFrameBuffer:
+width .req r0
+height .req r1
+bitDepth .req r2
+cmp width,#4096
+cmpls height,#4096
+cmpls bitDepth,#32
+result .req r0
+movhi result,#0
+movhi pc,lr
+
+fbInfoAddr .req r3
+push {lr}
+ldr fbInfoAddr,=FrameBufferInfo
+str width,[fbInfoAddr,#0]
+str height,[fbInfoAddr,#4]
+str width,[fbInfoAddr,#8]
+str height,[fbInfoAddr,#12]
+str bitDepth,[fbInfoAddr,#20]
+.unreq width
+.unreq height
+.unreq bitDepth
+
+mov r0,fbInfoAddr
+add r0,#0x40000000
+mov r1,#1
+bl MailboxWrite
+
+mov r0,#1
+bl MailboxRead
+
+teq result,#0
+movne result,#0
+popne {pc}
+
+mov result,fbInfoAddr
+pop {pc}
+.unreq result
+.unreq fbInfoAddr
