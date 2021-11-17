7735929442fb76bc9cb7548bd3dd02da5613d317
3dcoolcat <82654465+3dcoolcat@users.noreply.github.com>
2021-11-16 19:07:51 -0600

    Update main.s

--- main.s
+++ main.s
@@ -43,6 +43,55 @@ main:
 */
 mov sp,#0x8000
 
+mov r0,#1024
+mov r1,#768
+mov r2,#16
+bl InitialiseFrameBuffer
+
+teq r0,#0
+bne noError$
+
+mov r0,#16
+mov r1,#1
+bl SetGpioFunction
+mov r0,#16
+mov r1,#0
+bl SetGpio
+
+error$:
+b error$
+
+noError$:
+fbInfoAddr .req r4
+mov fbInfoAddr,r0
+
+render$:
+fbAddr .req r3
+ldr fbAddr,[fbInfoAddr,#32]
+
+colour .req r0
+y .req r1
+mov y,#768
+drawRow$:
+x .req r2
+mov x,#1024
+drawPixel$:
+strh colour,[fbAddr]
+add fbAddr,#2
+sub x,#1
+teq x,#0
+bne drawPixel$
+
+sub y,#1
+add colour,#1
+teq y,#0
+bne drawRow$
+
+b render$
+
+.unreq fbAddr
+.unreq fbInfoAddr
+
 .globl GetMailboxBase
 GetMailboxBase:
 ldr r0,=0x2000B880
