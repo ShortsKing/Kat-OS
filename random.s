ddc65e27f0aeb74626d95e052d6e77472f532825
3dcoolcat <82654465+3dcoolcat@users.noreply.github.com>
2021-11-30 14:44:42 -0600

    Update random.s

--- random.s
+++ random.s
@@ -1,14 +1,30 @@
+/******************************************************************************
+*	random.s
+*	 by Alex Chadwick
+*
+*	A sample assembly code implementation of the screen02 operating system.
+*	See main.s for details.
+*
+*	random.s contains code to do with generating pseudo random numbers.
+******************************************************************************/
+
+/* NEW
+* Random is a function with an input of the last number it generated, and an 
+* output of the next number in a pseduo random number sequence.
+* C++ Signature: u32 Random(u32 lastValue);
+*/
 .globl Random
 Random:
-xnm .req r0
-a .req r1
+	xnm .req r0
+	a .req r1
+	
+	mov a,#0xef00
+	mul a,xnm
+	mul a,xnm
+	add a,xnm
+	.unreq xnm
+	add r0,a,#73
+	
+	.unreq a
+	mov pc,lr
 
-mov a,#0xef00
-mul a,xnm
-mul a,xnm
-add a,xnm
-.unreq xnm
-add r0,a,#73
-
-.unreq a
-mov pc,lr
