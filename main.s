eb2244b2e6ea156f98a906b8c960465411b2c3e1
3dcoolcat <82654465+3dcoolcat@users.noreply.github.com>
2021-11-30 15:01:50 -0600

    Update main.s

--- main.s
+++ main.s
@@ -2,8 +2,8 @@
 *	main.s
 *	 by Alex Chadwick
 *
-*	A sample assembly code implementation of the screen02 operating system, that 
-*	renders pseudo random lines to the screen.
+*	A sample assembly code implementation of the screen03 operating system, that 
+*	renders text to the screen
 *
 *	main.s contains the main operating system, and IVT code.
 ******************************************************************************/
@@ -73,53 +73,28 @@ main:
 	fbInfoAddr .req r4
 	mov fbInfoAddr,r0
 
-/* NEW
+/*
 * Let our drawing method know where we are drawing to.
 */
 	bl SetGraphicsAddress
 	
-	lastRandom .req r7
-	lastX .req r8
-	lastY .req r9
-	colour .req r10
-	x .req r5
-	y .req r6
-	mov lastRandom,#0
-	mov lastX,#0
-	mov r9,#0
-	mov r10,#0
-render$:
-	mov r0,lastRandom
-	bl Random
-	mov x,r0
-	bl Random
-	mov y,r0
-	mov lastRandom,r0
+/* NEW
+* Find the cmdline tag.
+*/
+	mov r0,#9
+	bl FindTag
 
-	mov r0,colour
-	add colour,#1
-	lsl colour,#16
-	lsr colour,#16
-	bl SetForeColour
-		
-	mov r0,lastX
-	mov r1,lastY
-	lsr r2,x,#22
-	lsr r3,y,#22
+/* NEW
+* Draw our command line.
+*/
+	ldr r1,[r0]
+	lsl r1,#2
+	sub r1,#8
+	add r0,#8
 
-	cmp r3,#768
-	bhs render$
-	
-	mov lastX,r2
-	mov lastY,r3
-	 
-	bl DrawLine
+	mov r2,#0
+	mov r3,#0
+	bl DrawString
 
-	b render$
-
-	.unreq x
-	.unreq y
-	.unreq lastRandom
-	.unreq lastX
-	.unreq lastY
-	.unreq colour
+loop$:
+	b loop$
