946cf3ad6555942e607eb710856f838c5a8e377a
3dcoolcat <82654465+3dcoolcat@users.noreply.github.com>
2021-11-30 14:39:46 -0600

    Update drawing.s

--- drawing.s
+++ drawing.s
@@ -1,23 +1,48 @@
+*
+*	A sample assembly code implementation of the screen02 operating system.
+*	See main.s for details.
+*
+*	drawing.s contains code to do with drawing shapes to the screen.
+******************************************************************************/
+
+/* NEW
+* The foreColour is the colour which all our methods will draw shapes in.
+* C++ Signature: short foreColour;
+*/
 .section .data
 .align 1
 foreColour:
-.hword 0xFFFF
+	.hword 0xFFFF
 
+/* NEW
+* graphicsAddress stores the address of the frame buffer info structure. 
+* C++ Signature: FrameBuferDescription* graphicsAddress;
+*/
 .align 2
 graphicsAddress:
-.int 0
+	.int 0
 
+/* NEW
+* SetForeColour changes the current drawing colour to the 16 bit colour in r0.
+* C++ Signature: void SetForeColour(u16 colour);
+*/
 .section .text
 .globl SetForeColour
 SetForeColour:
-cmp r0,#0x10000
-movhs pc,lr
-ldr r1,=foreColour
-strh r0,[r1]
-mov pc,lr
+	cmp r0,#0x10000
+	movhi pc,lr
+	moveq pc,lr
 
+	ldr r1,=foreColour
+	strh r0,[r1]
+	mov pc,lr
+
+/* NEW
+* SetGraphicsAddress changes the current frame buffer information to 
+* graphicsAddress;
+* C++ Signature: void SetGraphicsAddress(FrameBuferDescription* value);
+*/
 .globl SetGraphicsAddress
 SetGraphicsAddress:
-ldr r1,=graphicsAddress
-str r0,[r1]
-mov pc,lr
+	ldr r1,=graphicsAddress
+	str r0,[r1]
