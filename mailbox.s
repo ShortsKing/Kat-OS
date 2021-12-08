86a890fa157e0eda2a5f1430a57a0d546a867060
3dcoolcat <82654465+3dcoolcat@users.noreply.github.com>
2021-12-08 14:46:08 -0600

    Update mailbox.s

--- mailbox.s
+++ mailbox.s
@@ -2,7 +2,7 @@
 *	mailbox.s
 *	 by Alex Chadwick
 *
-*	A sample assembly code implementation of the screen03 operating system.
+*	A sample assembly code implementation of the screen04 operating system.
 *	See main.s for details.
 *
 *	mailbox.s contains code that interacts with the mailbox for communication
