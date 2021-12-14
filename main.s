cb248243132781cfd8cbce1cccb95bc614307362
3dcoolcat <82654465+3dcoolcat@users.noreply.github.com>
2021-12-13 19:04:21 -0600

    Update main.s

--- main.s
+++ main.s
@@ -17,102 +17,120 @@
 * allows simulators to run the elf, and also stops us getting a linker warning
 * about having no entry point. 
 */
+#include "mm.h"
+
 .section ".text.boot"
+
 .globl _start
 _start:
+    mrs    x0, mpidr_el1        
+    and    x0, x0,#0xFF        // Check processor id
+    cbz    x0, master        // Hang for all non-primary CPU
+    b    proc_hang
 
-/*
-* Branch to the actual main code.
-*/
-b main
+proc_hang: 
+    b proc_hang
 
-/*
-* This command tells the assembler to put this code with the rest.
-*/
-.section .text
+master:
+    adr    x0, bss_begin
+    adr    x1, bss_end
+    sub    x1, x1, x0
+    bl     memzero
 
-/*
-* main is what we shall call our main operating system method. It never 
-* returns, and takes no parameters.
-* C++ Signature: void main(void)
-*/
-main:
+	mov    sp, #LOW_MEMORY
+	bl    kernel_main
+	/*
+	* Branch to the actual main code.
+	*/
+	b main
 
-/*
-* Set the stack point to 0x8000.
-*/
-	mov sp,#0x8000
+	/*
+	* This command tells the assembler to put this code with the rest.
+	*/
+	.section .text
 
-/* 
-* Setup the screen.
-*/
-	mov r0,#1024
-	mov r1,#768
-	mov r2,#16
-	bl InitialiseFrameBuffer
+	/*
+	* main is what we shall call our main operating system method. It never 
+	* returns, and takes no parameters.
+	* C++ Signature: void main(void)
+	*/
+	main:
 
-/* 
-* Check for a failed frame buffer.
-*/
-	teq r0,#0
-	bne noError$
-		
-	mov r0,#16
-	mov r1,#1
-	bl SetGpioFunction
+	/*
+	* Set the stack point to 0x8000.
+	*/
+		mov sp,#0x8000
 
-	mov r0,#16
-	mov r1,#0
-	bl SetGpio
+	/* 
+	* Setup the screen.
+	*/
+		mov r0,#1024
+		mov r1,#768
+		mov r2,#16
+		bl InitialiseFrameBuffer
 
-	error$:
-		b error$
+	/* 
+	* Check for a failed frame buffer.
+	*/
+		teq r0,#0
+		bne noError$
 
-	noError$:
+		mov r0,#16
+		mov r1,#1
+		bl SetGpioFunction
 
-	fbInfoAddr .req r4
-	mov fbInfoAddr,r0
+		mov r0,#16
+		mov r1,#0
+		bl SetGpio
 
-/*
-* Let our drawing method know where we are drawing to.
-*/
-	bl SetGraphicsAddress
-	
-	mov r4,#0
-loop$:
-	ldr r0,=format
-	mov r1,#formatEnd-format
-	ldr r2,=formatEnd
-	lsr r3,r4,#4
-	push {r3}
-	push {r3}
-	push {r3}
-	push {r3}
-	bl FormatString
-	add sp,#16
-	
-	mov r1,r0
-	ldr r0,=formatEnd
-	mov r2,#0
-	mov r3,r4
-	
-	cmp r3,#768-16
-	subhi r3,#768
-	addhi r2,#256
-	cmp r3,#768-16
-	subhi r3,#768
-	addhi r2,#256
-	cmp r3,#768-16
-	subhi r3,#768
-	addhi r2,#256
+		error$:
+			b error$
+
+		noError$:
+
+		fbInfoAddr .req r4
+		mov fbInfoAddr,r0
+
+	/*
+	* Let our drawing method know where we are drawing to.
+	*/
+		bl SetGraphicsAddress
+
+		mov r4,#0
+	loop$:
+		ldr r0,=format
+		mov r1,#formatEnd-format
+		ldr r2,=formatEnd
+		lsr r3,r4,#4
+		push {r3}
+		push {r3}
+		push {r3}
+		push {r3}
+		bl FormatString
+		add sp,#16
+
+		mov r1,r0
+		ldr r0,=formatEnd
+		mov r2,#0
+		mov r3,r4
+
+		cmp r3,#768-16
+		subhi r3,#768
+		addhi r2,#256
+		cmp r3,#768-16
+		subhi r3,#768
+		addhi r2,#256
+		cmp r3,#768-16
+		subhi r3,#768
+		addhi r2,#256
 
 
-	bl DrawString
+		bl DrawString
 
-	add r4,#16
-	b loop$
+		add r4,#16
+		b loop$
 
-.section .data
-format:
-.ascii "%d=0b%b=0x%x=0%o='%c'"
-formatEnd:
+	.section .data
+	format:
+	.ascii "%d=0b%b=0x%x=0%o='%c'"
+	formatEnd:
