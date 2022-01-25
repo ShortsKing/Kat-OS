a9e0aee3c51665558aed96dfc9a4e0c6a5360488
ShortsKing <82279627+ShortsKing@users.noreply.github.com>
2022-01-25 09:22:58 -0600

    Update screen.s

--- screen.s
+++ screen.s
@@ -2,12 +2,12 @@
 
 _refreshScreen:
   
-  MOV PC, LR
+  MOV PC, LR @end subroutine
 
 .data
-screen: .ascii ""
-p1: .ascii "  "
-p2: .ascii "░░"
-p3: .ascii "▒▒"
-p4: .ascii "▓▓"
-p5: .ascii "██"
+screen: .ascii "" @ store uncompressed pixel data
+p0: .ascii "  " @ space, ascii 32
+p1: .ascii "░░" @ low density dotted, ascii 172
+p2: .ascii "▒▒" @ medium density dotted, ascii 173
+p3: .ascii "▓▓" @ high density dotted, ascii 174
+p4: .ascii "██" @ block, ascii 219
