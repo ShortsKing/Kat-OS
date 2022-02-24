1ebaacf1a884f471d5496a2d219b529ebac9729e
3dcoolcat <82654465+3dcoolcat@users.noreply.github.com>
2022-02-24 07:40:45 -0600

    Update parentCall.s

--- parentCall.s
+++ parentCall.s
@@ -1,43 +1,21 @@
+@this file will save current register values by writing them to a file called data.txt
+@if a file called data.txt is not found it will return an error
   .global _start
 _start:
-
-save:
-  STM {R4-R12}
-  LDM {R12}
-  STR R12,=string
-  BL write
-  LDM {R11}
-  STR R11,=string
-  BL write
-  LDM {R10}
-  STR R10,=string
-  BL write
-  LDM {R9}
-  STR R9,=string
-  BL write
-  LDM {R8}
-  STR R8,=string
-  BL write
-  LDM {R7}
-  STR R7,=string
-  BL write
-  LDM {R6}
-  STR R6,=string
-  BL write
-  LDM {R5}
-  STR R5,=string
-  BL write
-  LDM {R4}
-  STR R4,=string
-  BL write
-
-write:
-  MOV R7, #3
-  
-
-exit:
-  MOV R7, #1
+  LDR R0,=data.txt
+  MOV R1, #o_rdonly
+  MOV R2, #rdwr
+  MOV R7, #sys_open
   SWI 0
   
-.data
-.ascii "                    \n"
+  MOVS R8, R0
+  BPL writebuffer
+  MOV R0, #1
+  LDR R1,=error1
+  MOV R2, #18
+  MOV R7, #4
+  SWI 0
+  B   exit
+  
+writebuffer:
+  MOV R0, R9
