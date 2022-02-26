75367b32f06fa667178d1e75bac827fa70be56d5
3dcoolcat <82654465+3dcoolcat@users.noreply.github.com>
2022-02-26 08:59:33 -0600

    Update parentCall.s
    John, please read through this and check for errors

--- parentCall.s
+++ parentCall.s
@@ -2,6 +2,7 @@
 @if a file called data.txt is not found it will return an error
   .global _start
 _start:
+  PUSH {R4-R12}
   LDR R0,=data.txt
   MOV R1, #o_rdonly
   MOV R2, #rdwr
@@ -15,11 +16,40 @@ _start:
   MOV R2, #18
   MOV R7, #4
   SWI 0
-  B   exit
+  B   finish
+
+store:
+  POP {R12}
+  STR R12,=regvals
+  BL writebuffer
+  POP {R11}
+  STR R11,=regvals
+  BL writebuffer
+  POP {R10}
+  STR R10,=regvals
+  BL writebuffer
+  POP {R9}
+  STR R9,=regvals
+  BL writebuffer
+  POP {R8}
+  STR R8,=regvals
+  BL writebuffer
+  POP {R7}
+  STR, R7,=regvals
+  BL writebuffer
+  POP {R6}
+  STR R6,=regvals
+  BL writebuffer
+  POP {R5}
+  STR R5,=regvals
+  BL writebuffer
+  POP {R4}
+  STR R4,=regvals
+  BL writebuffer
   
 writebuffer:
   MOV R0, R9
-  LDR R1,regvals
+  LDR R1,=regvals
   MOV R2, #16
   MOV R7, #4
   SWI 0
@@ -38,4 +68,4 @@ finish:
   SWI 0
   
 .data
-regvals: "aaaaaaaaaaaaaaaa"
+regvals: "                \n"
