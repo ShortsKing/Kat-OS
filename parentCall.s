b9b2d86ae3c56a1cd3410ef411fa3594deb71de9
3dcoolcat <82654465+3dcoolcat@users.noreply.github.com>
2022-02-28 07:44:59 -0600

    please read the description
    does LDR load a register with an address or load a register into an address

--- parentCall.s
+++ parentCall.s
@@ -2,11 +2,11 @@
 @if a file called data.txt is not found it will return an error
   .global _start
 _start:
-  PUSH {R4-R12}
-  LDR R0,=data.txt
-  MOV R1, #o_rdonly
-  MOV R2, #rdwr
-  MOV R7, #sys_open
+  PUSH {R4-R12} @pushes the data in registers 4-12 to the stack
+  LDR R0,=data.txt @loads R0 with the address of the data file
+  MOV R1, #0 @flag read only file
+  MOV R2, #0666
+  MOV R7, #5
   SWI 0
   
   MOVS R8, R0
@@ -18,7 +18,7 @@ _start:
   SWI 0
   B   finish
 
-store:
+store:  @ignore this section as it is still under developement because I am confused as to when I should use LDR and when I should use STR
   POP {R12}
   STR R12,=regvals
   BL writebuffer
@@ -47,14 +47,13 @@ store:
   STR R4,=regvals
   BL writebuffer
   
-writebuffer:
-  MOV R0, R9
-  LDR R1,=regvals
-  MOV R2, #16
-  MOV R7, #4
-  SWI 0
-  MOV R1, #0
+writebuffer: 
+  LDR R1,=regvals @loads R1 with the string at regvals
+  MOV R2, #16 @string is 16 characters long
+  MOV R7, #4 @system write
+  SWI 0 @interrupt
 
+@flush and close data.txt
   MOV R0, R8
   MOV R7, #118
   SWI 0
@@ -63,9 +62,10 @@ writebuffer:
   SWI 0
   
 finish:
-  MOV R0, #0
-  MOV R7, #1
+  MOV R0, #0 @0 return code
+  MOV R7, #1 @sys exit
   SWI 0
   
 .data
-regvals: "                \n"
+data.txt: .asciz "data.txt" @file name
+regvals: .asciz "                \n" @string
