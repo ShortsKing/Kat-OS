9b422c0a4e3137b588b147a4b71a2a53777fbbca
SuperSiliconosis123 <91914918+SuperSiliconosis123@users.noreply.github.com>
2022-03-03 15:22:34 -0600

    Update Init.c

--- Init.c
+++ Init.c
@@ -2,14 +2,57 @@
 #include <unistd.h>
 #include <stdio.h>
 
-main() {
+void Kat-OS() {
    pid_t pid;
-   char *const parmList[] = {"/bin/ls", "-l", "/u/userid/dirname", NULL};
+   char *const parmList[] = {NULL};
 
-   if ((pid = fork()) == -1)
-     printf("fork error\nKat-OS is Inoperable");
+   if ((pid = fork()) == -1) printf("fork error\nKat-OS has been Rendered Inoperable\n\nPlease Cut Power"); //Check if fork failed
    else if (pid == 0) {
-      execv("/Kat-OS/Kat-OS", parmList);
-      printf("Return not expected. Must be an execv error.n");
+      execv("/Kat-OS/Kat-OS", parmList); 
+      printf("Return not expected. Must be an execv error\nKat-OS has been Rendered Inoperable\n\nPlease Cut Power");
    }
+}
+
+void Shell() {
+   pid_t pid;
+   char *const parmList[] = {NULL};
+
+   if ((pid = fork()) == -1) printf("fork error\nKat-OS has been Rendered Inoperable\n\nPlease Cut Power"); //Check if fork failed
+   else if (pid == 0) {
+      execv("/Kat-OS/Shell", parmList); 
+      printf("Return not expected. Must be an execv error\nKat-OS has been Rendered Inoperable\n\nPlease Cut Power");
+   }
+}
+
+void User() {
+   pid_t pid;
+   char *const parmList[] = {NULL};
+
+   if ((pid = fork()) == -1) printf("fork error\nKat-OS has been Rendered Inoperable\n\nPlease Cut Power"); //Check if fork failed
+   else if (pid == 0) {
+      execv("/Kat-OS/User", parmList); 
+      printf("Return not expected. Must be an execv error\nKat-OS has been Rendered Inoperable\n\nPlease Cut Power");
+   }
+}
+
+void Programs() {
+   pid_t pid;
+   char *const parmList[] = {NULL};
+
+   if ((pid = fork()) == -1) printf("fork error\nKat-OS has been Rendered Inoperable\n\nPlease Cut Power"); //Check if fork failed
+   else if (pid == 0) {
+      execv("/Kat-OS/Programs", parmList); 
+      printf("Return not expected. Must be an execv error\nKat-OS has been Rendered Inoperable\n\nPlease Cut Power");
+   }
+}
+
+void Init() {
+   Kat-OS();
+   Shell();
+   User();
+   Programs();
+}
+
+main() {
+   Init();
 }
