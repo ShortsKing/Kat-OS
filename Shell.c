f33f8327e217c20c83a33d83a5cb0fa31085ad24
3dcoolcat <82654465+3dcoolcat@users.noreply.github.com>
2022-03-14 15:20:10 -0500

    Update Shell.c

--- Shell.c
+++ Shell.c
@@ -1,9 +1,29 @@
-#include <stdio.h>
-#include <sys/types.h>
+#include <stdio.h> /*adds stdio to file*/
+#include <sys/types.h> /*adds sys/types to file*/
+#include <unistd.h> /*adds unistd to file*/
+#include <dirent.h> /*adds dirent to file*/
+int main() { /*initializes the file*/
+  char str[100]; /*creates a 100 character variable*/
+  
+  printf("please enter a command :"); /*asks you to enter a command*/
+  gets( str );
+  
+  printf("\nyou entered: "); /*returns what you entered*/
+  puts( str );
+  
+  DIR *folder;
 
-int main() 
-{
-char command;
+  folder = opendir(str);
+  if(folder == NULL)
+  {
+  puts("Unable to read directory");
+      return(1);
+  }
+  else
+  {
+      puts("Directory is opened!");
+  }
+  closedir(folder);
 
-scanf("%s", &command);
+  return(0);
 }
