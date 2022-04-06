9d23a40499291bc87c257c015590ab88ebe2762f
SuperSiliconosis123 <91914918+SuperSiliconosis123@users.noreply.github.com>
2022-04-06 15:07:00 -0500

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -11,5 +11,11 @@ int main() {
 	usleep(5 * second); //sleeps for 5 second
 	cout << "JUST KIDDING!!\n";
 	cout << "Cat or Human? (H/C): ";
-	cin >> toupper(CatOrHuman);
+	cin >> tolower(CatOrHuman);
+	switch(CatOrHuman) {
+		case 'c':
+			execv(kittyEXE);
+		case 'h':
+			fork() etc etc etc
+	}
 }
