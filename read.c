5be8e8e55e4e7b9d379c4c5a592ce600529b3810
ShortsKing <82279627+ShortsKing@users.noreply.github.com>
2022-04-21 18:10:13 -0500

    Delete read.c

--- read.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main{
-  for(i = 1; i < 100; ++i){
-    
-    printf("John loves reading and he pets the koot koot 1 million times in one minute, he also hates mcdonalds.");
-  }
-
-}
