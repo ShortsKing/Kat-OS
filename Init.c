91c5993c5f34662fed31f5715563371e740ca999
ShortsKing <82279627+ShortsKing@users.noreply.github.com>
2022-04-21 18:08:48 -0500

    Update Init.c

--- Init.c
+++ Init.c
@@ -22,7 +22,6 @@ void Init() {
    exec(/Kat-OS/User); //execute /Kat-OS/User
    exec(/Kat-OS/Shell); // ...
    exec(/Kat-OS/Programs); // ...
-   exec(/Kat-OS/read);//you have no idea how alone you are garfeild
 }
 main() {
    Init(); //Call Init()
