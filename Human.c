b6662a2ae8b9dc87993f49c35d25050c0c7488c5
ShortsKing <82279627+ShortsKing@users.noreply.github.com>
2022-04-22 16:41:58 -0500

    Update Human.c

--- Human.c
+++ Human.c
@@ -1,3 +1,11 @@
+/* This program reads settings.txt, and based on line 1 (shell vs. desktop)
+ * executes either the shell program, or continues by forking and executing 
+ * a desktop program, while also launching the applications program and 
+ * managing everything from the background.
+ * 
+ * i am currently tied up in other business. wouldn't mind some help (*COUGH* joseph *COUGH*)
+ */
+
 #include <stdio.h>
 
 char readSetting(int line;){
