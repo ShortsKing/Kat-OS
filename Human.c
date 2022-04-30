b5e8952fd7f40809d1438a950856e2e5e8cf2153
ShortsKing <82279627+ShortsKing@users.noreply.github.com>
2022-04-30 12:29:50 -0500

    Update Human.c

--- Human.c
+++ Human.c
@@ -8,6 +8,17 @@
 //heeheehee
 #include <stdio.h>
 
+void exec(char path[], ) {
+   pid_t pid; //i think pid_t relies on sys/types.h? just a guess, based on the name (types.h)
+   char *const parmList[] = {NULL};
+
+   if ((pid = fork()) == -1) printf("fork error\nKat-OS has been Rendered Inoperable\n\nPlease Cut Power"); //Check if fork failed
+   else if (pid == 0) {
+      execv(path, parmList); //runs main() function in program located at $path with arguments $parmList, which should be a NULL terminated string
+      printf("Return not expected. Must be an execv error\nKat-OS has been Rendered Inoperable\n\nPlease Cut Power"); //This will only run if execv() failed, because execv() should terminate the existing process
+   }
+}
+
 char readSetting(char setting[]; int s_len;){
   FILE* ptr;
   char str[20000];
@@ -41,9 +52,9 @@ char readSetting(char setting[]; int s_len;){
 int main(){
   char ShellOrDesktop = readSetting("Shell/Desktop", 13);
   switch(ShellOrDesktop){
-    case 's': 
-    case 'S':
-    case 'd':
-    case 'D':
+    case 's': exec("Shell");
+    case 'S': exec("Shell");
+    case 'd': exec("Desk");
+    case 'D': exec("Desk");
   }
 }
