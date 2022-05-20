8c94051eb2b6be4430d96b69e4cdc82bd2267b58
SuperSiliconosis123 <91914918+SuperSiliconosis123@users.noreply.github.com>
2022-05-20 12:15:28 -0500

    Update Shell.c

--- Shell.c
+++ Shell.c
@@ -5,8 +5,6 @@
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/wait.h>
-#include<readline/readline.h>
-#include<readline/history.h>
   
 #define MAXCOM 1000 // max number of letters to be supported
 #define MAXLIST 100 // max number of commands to be supported
@@ -32,18 +30,9 @@ void init_shell()
 }
   
 // Function to take input
-int takeInput(char* str)
-{
-    char* buf;
-  
-    buf = readline("\n>>> ");
-    if (strlen(buf) != 0) {
-        add_history(buf);
-        strcpy(str, buf);
-        return 0;
-    } else {
-        return 1;
-    }
+int takeInput(char* str) {
+  printf("\n>>>");
+  scanf("%d", &str)
 }
   
 // Function to print Current Directory.
